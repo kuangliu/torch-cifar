d214f6d3d098bfa3afefb7e83745231366781944
Mrliukuang <kuang.liu@hotmail.com>
2016-06-12 10:35:21 +0800

    Tidy up

--- resnet.lua
+++ resnet.lua
@@ -7,7 +7,7 @@ AvgPool = nn.SpatialAveragePooling
 BN = nn.SpatialBatchNormalization
 
 local shortCutType = 'CONV' or 'ZERO_PAD'
-local blobType = 'NIN' or 'BASIC'
+local blockType = 'BOTTLENECK' or 'BASIC'
 
 
 function shortCut(nInputPlane, nOutputPlane, stride)
@@ -35,14 +35,15 @@ function shortCut(nInputPlane, nOutputPlane, stride)
 end
 
 
-function basicBlob(nConvPlane, stride)
-    -----------------------------------------------------------------------
-    -- The basic blob of ResNet: a normal small CNN + an Identity shortcut.
+function basicblock(nConvPlane, stride)
+    ---------------------------------------------------------------------------
+    -- The basic block of ResNet: a 2 CONV layer CNN + an Identity shortcut.
     -- In torch we use `ConCatTable` to implement.
     -- Inputs:
     --      - nConvPlane: CONV channels
     --      - stride: CONV stride
-    -----------------------------------------------------------------------
+    -- Note the 2 CONV layers share the same CONV channels.
+    ---------------------------------------------------------------------------
     local nInputPlane = nPlane
     nPlane = nConvPlane
 
@@ -62,10 +63,10 @@ function basicBlob(nConvPlane, stride)
 end
 
 
-function ninBlob(nFirstConvPlane, stride)
+function bottleneck(nFirstConvPlane, stride)
     -----------------------------------------------------------------------
-    -- Network in Network blob.
-    -- NIN follows the CNN structure:
+    -- Bottlenect block.
+    -- which follows the structure:
     --      1. 1x1 CONV with nFirstConvPlane channels
     --      2. 3x3 CONV with nFirstConvPlane channels
     --      3. 1x1 CONV with 4*nFirstConvPlane channles
@@ -75,7 +76,7 @@ function ninBlob(nFirstConvPlane, stride)
     -----------------------------------------------------------------------
     local nInputPlane = nPlane              -- intput data channels
     local nOutputPlane = 4*nFirstConvPlane  -- first conv layer channels
-    nPlane = nOutputPlane                   -- nPlane flow between blobs
+    nPlane = nOutputPlane                   -- nPlane flow between blocks
 
     local s = nn.Sequential()
     s:add(Conv(nInputPlane,nFirstConvPlane,1,1,1,1,0,0))
@@ -96,34 +97,34 @@ function ninBlob(nFirstConvPlane, stride)
 end
 
 
-function nBlob(nFirstConvPlane, n, stride)
+function nBlock(nFirstConvPlane, n, stride)
     -------------------------------------------------------------------------
-    -- Stack n basic/NIN blobs together
+    -- Stack n basic/bottleneck blocks together
     -- Inputs:
     --      - nFirstConvPlane: CONV channels of the first CONV layer
-    --          - For basicBlob: all CONV layers in the nBlob share the same CONV channels (=nFirstConvPlane)
-    --          - For ninBlob:
-    --              - nSecondConvPlane = nFirstConvPlan
+    --          - For basic block: all CONV layers in the nBlock share the same CONV channels (=nFirstConvPlane)
+    --          - For bottlenect block:
+    --              - nSecondConvPlane = nFirstConvPlane
     --              - nThirdConvPlane = 4*nFirstConvPlane
-    --      - n: # of blobs
-    --      - stride: CONV stride of first blob, could be 1 or 2
+    --      - n: # of blocks
+    --      - stride: CONV stride of first block, could be 1 or 2
     --          - stride=1: maintain input H&W
     --          - stride=2: decrease input H&W by half
     -------------------------------------------------------------------------
-    local blob
-    if blobType == 'BASIC' then
-        blob = basicBlob
-    elseif blobType == 'NIN' then
-        blob = ninBlob
+    local block
+    if blockType == 'BASIC' then
+        block = basicblock
+    elseif blockType == 'BOTTLENECK' then
+        block = bottleneck
     else
-        error('Unknown blobType..'..blobType)
+        error('Unknown blockType..'..blockType)
     end
 
     local s = nn.Sequential()
-    s:add(blob(nFirstConvPlane, stride))
+    s:add(block(nFirstConvPlane, stride))
 
     for i = 2,n do
-        s:add(blob(nFirstConvPlane, 1))
+        s:add(block(nFirstConvPlane, 1))
     end
 
     return s
@@ -143,14 +144,9 @@ function getResNet()
 
     nPlane = 64
 
-    -- net:add(nBlob(16,3,1))
-    -- net:add(nBlob(32,3,2))
-    -- net:add(nBlob(64,3,2))
-    --net:add(nBlob(64,64,3,2))
-
-    net:add(nBlob(64,3,1))
-    net:add(nBlob(128,3,2))
-    net:add(nBlob(256,3,2))
+    net:add(nBlock(64,3,1))
+    net:add(nBlock(128,3,2))
+    net:add(nBlock(256,3,2))
 
     net:add(AvgPool(8,8,1,1))
     net:add(nn.View(1024):setNumInputDims(3))
